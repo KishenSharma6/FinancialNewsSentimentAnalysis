066656ab4897abc9c45f512922e2a9397f0a8b21
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-04-06 18:44:30 -0700

    Added Preprocessor class

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,18 +1,26 @@
+import nltk
+
 class Preprocessor:
     def __init__(self, rawData):
         self.rawData= rawData
     
-    def normalize(self):
-
+    def normalization(self):
         temp= self.rawData.copy()
         temp= temp.str.lower()
-        temp= temp.str.replace('[^\w\s]','')
-        #convert strings to numbers
-        
-        self.processedData= temp
+        temp= temp.str.replace('[^\w\s]','', regex= True)
+        self.normalizedData= temp
     
     def tokenization(self):
-        pass
+        tokens= self.normalizedData.str.split()
+        self.tokens= tokens
+   
+    def lemmatization(self):
+        self.lemmaTokens= self.tokens.apply(lemmatize_text)
 
-    def lemmatization():
-        pass
+class DataRepresentation:
+    pass
+
+
+def lemmatize_text(text):
+    lemmatizer= nltk.stem.WordNetLemmatizer()
+    return [lemmatizer.lemmatize(w) for w in text]
