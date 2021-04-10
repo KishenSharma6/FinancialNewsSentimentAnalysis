ee5d498920ab9bac16dd20dee29136c0cb255b27
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-04-10 16:23:40 -0700

    Minor changes

---
+++ src/metrics/metrics.py
@@ -0,0 +1,12 @@
+import sklearn
+
+class Metrics:
+    def __init__(self, predictions, actual):
+        self.yhat= predictions
+        self.y= actual
+
+    def confusion_matrix(self):
+        """Prints the confusion matrix of the classification
+        """
+        from sklearn.metrics import confusion_matrix
+        print(confusion_matrix(self.y, self.yhat))
