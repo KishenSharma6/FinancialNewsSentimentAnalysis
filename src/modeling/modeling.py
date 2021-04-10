5e03f84f92fc447dfb1f4e9f6a2e7534a45ed548
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-04-10 16:22:06 -0700

    Added base_model_evaluation

--- src/modeling/modeling.py
+++ src/modeling/modeling.py
@@ -1,15 +1,15 @@
 import sklearn
+import numpy as np
 
 class Models:
     def __init__(self, training_data, target):
         self.training= training_data
         self.target= target
     
-
-    #Initialize base models
     
     
     def base_model_evaluation(self):
+        #Initilialize Base Models
         from sklearn.linear_model import LogisticRegression
         from sklearn.ensemble import RandomForestClassifier
         from sklearn.neighbors import KNeighborsClassifier
@@ -20,6 +20,11 @@ class Models:
         knn= KNeighborsClassifier(n_jobs= -1)
         linearSVC= LinearSVC(multi_class= "crammer_singer", random_state= 24)
         
-        baseModels= [ logR, randomF, knn, linearSVC]
+        from sklearn.model_selection import cross_val_score
+        baseModels= [logR, randomF, knn, linearSVC]
         for model in baseModels:
-            print(model)
+            scores= cross_val_score(model, self.training, self.target, cv=5)
+            print("=========================")
+            print("%s Scores: %s" % (model, scores))
+            print("Average Sccore: %s" % (round(np.mean(scores),2)))
+            print("Standard Deviations: %s" % (round(np.std(scores),2)))
